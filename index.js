0bbee7c08f9b158bf3fdcdb7d13f9785fd55bfad
devops056 <64005717+devops056@users.noreply.github.com>
2020-09-16 11:29:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = '<h1>Hello Node1234 5678!\n</h1>'
+  const msg = '<h1>Hello Node1234 5678!git push \n</h1>'
   res.end(msg);
 });
 
